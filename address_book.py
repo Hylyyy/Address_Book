13c50dd2bf89f1561de58250da58819176d4aea6
Hylyyy <palisochydeelyn97@gmail.com>
2023-02-13 23:30:10 +0800

    user choices

--- address_book.py
+++ address_book.py
@@ -1,2 +1,21 @@
+# This code is used to create an Adress Book that can contain 100 entries.
+
+# These are the input, processing and outputs statements.
 print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
+
+# The user will be ask, what he or she wanted to do.
+
+    print(
+     """
+-----------------------------------
+|    What would you like to do?   |
+|---------------------------------|
+|    1. Add Contact               |
+|    2. Edit Contact              |
+|    3. Delete Contact            |
+|    4. View Contacts             |
+|    5. Search Address Book       |
+|    6. Exit                      |
+-----------------------------------
+    """)
