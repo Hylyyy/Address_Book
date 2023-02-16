521e2650a66e8678c042014be38d7724adf05481
Hylyyy <palisochydeelyn97@gmail.com>
2023-02-16 14:35:32 +0800

    carrier

--- address_book.py
+++ address_book.py
@@ -224,6 +224,8 @@ while count <= 100:
         pepnumber = phonenumbers.parse(number)
         location = geocoder.description_for_number(pepnumber, "en")
         print(location)
+        service_pro = phonenumbers.parse(number)
+        print(carrier.name_for_number(service_pro, "en"))
 
 # If the user chooses option 6, the program will exit.
     elif option == 7:
